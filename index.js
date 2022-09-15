0a3e5721da2765eab14d816116abfafd4e3ec6ba
Dubzy110 <Dubzy110@gmail.com>
2022-09-15 15:18:02 -0700

    added index files

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public', {}))
+
+let x = 0
+
+const items = {
+  1:"item 1",
+  2:"notItem",
+}
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/item/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id)
+  res.json(items[id])
+})
+
+app.get('/stuff', (req, res) => {
+  x++
+    res.send(x.toString())
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
