b61135f4dcce58b2689c44a271ce353717892d57
Alexander Phillips <alexphillipsdev@gmail.com>
2022-09-15 17:52:28 -0500

    adds item delete route

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.use(express.static('public', {}))
+app.use(express.static("public", {}));
 
-let x = 0
+let x = 0;
 
 const items = {
-  1:"item 1",
-  2:"notItem",
-}
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+  1: "item 1",
+  2: "notItem"
+};
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.get('/item/:id', (req, res) => {
+app.get("/item/:id", (req, res) => {
   const id = req.params.id;
-  console.log(id)
-  res.json(items[id])
-})
+  console.log(id);
+  res.json(items[id]);
+});
 
-app.get('/stuff', (req, res) => {
-  x++
-    res.send(x.toString())
-  })
+app.get("/stuff", (req, res) => {
+  x++;
+  res.send(x.toString());
+});
+
+app.delete("/item/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  delete items[id];
+  res.status(200).json({ text: `Successfully deleted item of id: ${id}!` });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
